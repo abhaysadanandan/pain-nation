ce10dc1741f50dcae2c26fc4193168892373d761
Amit Kothiyal <amit.in@awfis.com>
2017-01-08 00:19:19 +0530

    Login Screen FB and Gmail Buttons added

--- app/src/main/java/in/painnation/mobile/LoginActivity.java
+++ app/src/main/java/in/painnation/mobile/LoginActivity.java
@@ -1,10 +1,7 @@
 package in.painnation.mobile;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
