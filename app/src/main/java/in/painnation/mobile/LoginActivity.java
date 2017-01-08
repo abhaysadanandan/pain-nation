af1cc53ca4c5e6f27f4aa286c0aef5b35f5cec4c
Amit Kothiyal <amit.in@awfis.com>
2017-01-08 16:13:00 +0530

    Added the effect for button press on Login Screen

--- app/src/main/java/in/painnation/mobile/LoginActivity.java
+++ app/src/main/java/in/painnation/mobile/LoginActivity.java
@@ -1,8 +1,10 @@
 package in.painnation.mobile;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,6 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView txtWelcome=(TextView)findViewById(R.id.txtWelcome);
+        TextView txtPain=(TextView)findViewById(R.id.txtPain);
+        Typeface font = Typeface.createFromAsset(getAssets(), "Roboto-Bold.ttf");
+        txtPain.setTypeface(font);
+        txtWelcome.setTypeface(font);
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
